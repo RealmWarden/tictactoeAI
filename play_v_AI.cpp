95834bd897354acf72f834ca21b44baf36aa4c4c
RealmWarden <kcutchin@usc.edu>
2025-12-14 02:13:21 -0800

    added replayability to play_v_AI

--- play_v_AI.cpp
+++ play_v_AI.cpp
@@ -54,65 +54,88 @@ int main(){
         randomizeWeights(weightsLayerOne, weightsLayerTwo);
     }
 
-    /* create a 1x9 matrix of dashes */
-    initializeBoard(matrix);
+    /* time ot play against AI */
+    bool playAgain = true;
+    while (playAgain) {
+        /* create a 1x9 matrix of dashes */
+        initializeBoard(matrix);
 
-    /* print a 3x3 grid of dashes */
-    printBoard(matrix);
-
-    /* decide who is first */
-    bool usersTurn = true;
-    bool meFirst = false;
-    int coinFlip = rand() % 2;
-    if (coinFlip == 1){
-        usersTurn = false;
-        meFirst = true;
-    }
-
-    /* main loop */
-    for (int i = 0; i < 9; i++) {
-        /* if x is even, symbol is "x", otherwise "o" */
-        symbol = getSymbol(i);
-        
-        int userInput;
-        float inputLayer[10];
-        boardToInputLayer(matrix, inputLayer, meFirst);
-        
-        /* decide who is control */
-        if (usersTurn){
-            /* wait for the user to input a single-digit natural number */
-            userInput = getHumanMove(matrix, symbol);
-        } else {
-            /* THIS IS WHERE THE DECISION MAKING GOES */
-            float outputLayerOne[9];
-            passThroughLayerOne(inputLayer, weightsLayerOne, outputLayerOne);
-            float outputLayerTwo[9];
-            passThroughLayerTwo(outputLayerOne, weightsLayerTwo, outputLayerTwo);
-            userInput = getOutput(outputLayerTwo, matrix);
-        }
-        
-        /* flip turn identifier */
-        usersTurn = !usersTurn;
-
-        /* print the symbol in the correct position */
-        addMove(matrix, userInput, symbol);
+        /* print a 3x3 grid of dashes */
         printBoard(matrix);
 
-        /* check for a win */
+        /* decide who is first */
+        bool usersTurn = true;
+        bool meFirst = false;
+        int coinFlip = rand() % 2;
+        if (coinFlip == 1){
+            usersTurn = false;
+            meFirst = true;
+        }
+
+        /* main loop */
+        for (int i = 0; i < 9; i++) {
+            /* if x is even, symbol is "x", otherwise "o" */
+            symbol = getSymbol(i);
+            
+            int userInput;
+            float inputLayer[10];
+            boardToInputLayer(matrix, inputLayer, meFirst);
+            
+            /* decide who is control */
+            if (usersTurn){
+                /* wait for the user to input a single-digit natural number */
+                userInput = getHumanMove(matrix, symbol);
+            } else {
+                /* THIS IS WHERE THE DECISION MAKING GOES */
+                float outputLayerOne[9];
+                passThroughLayerOne(inputLayer, weightsLayerOne, outputLayerOne);
+                float outputLayerTwo[9];
+                passThroughLayerTwo(outputLayerOne, weightsLayerTwo, outputLayerTwo);
+                userInput = getOutput(outputLayerTwo, matrix);
+            }
+            
+            /* flip turn identifier */
+            usersTurn = !usersTurn;
+
+            /* print the symbol in the correct position */
+            addMove(matrix, userInput, symbol);
+            printBoard(matrix);
+
+            /* check for a win */
+            if (checkWin(matrix, symbol)) {
+                break;
+            }
+
+            /* check for a tie */
+            if (checkTie(matrix)) {
+                break;
+            }
+        }
+
         if (checkWin(matrix, symbol)) {
-            break;
+            cout << "The winner is: " << symbol << "!" << endl;
+        } else {
+            cout << "Tie!" << endl;
         }
 
-        /* check for a tie */
-        if (checkTie(matrix)) {
-            break;
+        cout << "Play again? (y/n): ";
+        char yn;
+        cin >> yn;
+        if (yn == 'y'){
+            playAgain = true;
+        } else if (yn == 'n') {
+            playAgain = false;
+        } else {
+            while (yn != 'n' && yn != 'y'){
+                cout << "Please enter y to play again or n to exit: ";
+                cin >> yn;
+            }
+            if (yn == 'y'){
+                playAgain = true;
+            } else if (yn == 'n') {
+                playAgain = false;
+            }
         }
-    }
-
-    if (checkWin(matrix, symbol)) {
-        cout << "The winner is: " << symbol << "!" << endl;
-    } else {
-        cout << "Tie!" << endl;
     }
 }
 
